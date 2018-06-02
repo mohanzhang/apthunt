d955c13e7dc337835d84d6476320841a3e85ff46
Mohan Zhang <mohanzhang@gmail.com>
2018-06-02 15:59:45 -0400

    Scraper works and updates airtable

---
+++ app/scripts/update.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+Airrecord.api_key = ENV.fetch("AIRTABLE_API_KEY")
+
+class Apt < Airrecord::Table
+  self.base_key = ENV.fetch("AIRTABLE_APP_KEY")
+  self.table_name = "Availabilities"
+end
+
+url = URI.join(ENV.fetch("BASE_URL"), '/floor-plans?unitrent_min=&unitrent_max=&bed_type%5B%5D=studio&bed_type%5B%5D=one&availability=0&floornumber_min=&floornumber_max=&wpv_filter_submit=Search')
+html = HTTParty.get(url)
+doc = Nokogiri::HTML(html)
+
+units = doc.css('.unit_details')
+
+css_lookup = [:unit_number, :bedrooms, :bath, :sq_feet, :price, :availability].map {|k| [k, ".#{k}"]}.to_h
+payload_to_css = {
+  "Unit Number" => :unit_number,
+  "Bedrooms" => :bedrooms,
+  "Bath" => :bath,
+  "SQ FT" => :sq_feet,
+  "Price" => :price,
+  "Availability" => :availability
+}
+
+units.each do |unit|
+  payload = {}
+  payload_to_css.each do |k,v|
+    payload[k] = unit.css(css_lookup.fetch(v)).text.strip
+  end
+
+  image_url = doc.css('img[alt="Unit ' + payload.fetch("Unit Number") + '"]').attr('src').text
+  payload["Floor Plan"] = image_url
+  record = Apt.new(payload)
+  record.create
+end
